a70aab10bfdc4ef1632f538106dd500fd244783a
Jonathan Wright <jonathanwright@Jonathans-iMac.local>
2018-09-08 11:26:02 +0100

    adding project start

---
+++ script.js
@@ -0,0 +1,42 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+//click on a list item and it strikethroughs the text
+function getEventTarget(e){
+	e = e || window.event;
+	return e.target || e.srcElement;
+}
+
+
+ul.onclick = function(event){
+	var target = getEventTarget(event);
+	target.classList.toggle("done");
+}
+
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
